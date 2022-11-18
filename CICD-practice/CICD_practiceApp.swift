260b7d7ec2fe969d3d80cfbf00c077e7910caf47
Terry-Koo <xpflxhfl94@naver.com>
2022-11-18 10:54:02 +0900

    Initial commit

---
+++ CICD-practice/CICD_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_practiceApp.swift
+//  CICD-practice
+//
+//  Created by Terry Koo on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct CICD_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICD-practice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CICD-practice
+//
+//  Created by Terry Koo on 2022/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
